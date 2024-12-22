8dbbcab6108ce97f40255b9996558e3d72d7e0ef
Vanquoc0201 <vovanquoc0201@gmail.com>
2024-12-22 14:29:15 +0700

    BaitapReactBuoi1

---
+++ BaiTapReactBuoi1/src/example-2/index.jsx
@@ -0,0 +1,15 @@
+import Footer from "./footer";
+import ListCard from "./listcard";
+import Header from "./header"
+import Carousel from "./carousel";
+
+export default function Example2() {
+  return (
+    <div>
+        <Header />
+        <Carousel />
+        <ListCard />
+        <Footer />
+    </div>
+  )
+}
---
+++ BaiTapReactBuoi1/src/example-2/listcard.jsx
@@ -0,0 +1,12 @@
+import Card from "./card"
+
+export default function () {
+  return (
+    <div class="grid grid-cols-4 container mx-auto gap-4 mt-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </div>
+  )
+}
